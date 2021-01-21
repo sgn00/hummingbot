b2f3e0d60da4a1fb7b5b2a3c85b6e4afa8bd1ee9
sg <a@a.com>
2021-01-21 05:28:44 -0800

    Set timers

--- hummingbot/strategy/pure_market_making/pure_market_making.pyx
+++ hummingbot/strategy/pure_market_making/pure_market_making.pyx
@@ -1161,6 +1161,7 @@ cdef class PureMarketMakingStrategy(StrategyBase):
                         )
      
                     ask_order_id = self.c_sell_with_specific_market(self._market_info, order.quantity)
+                    self.set_timers()
                     #self._filled_sells_balance += 1 # for pingpong
 
             
